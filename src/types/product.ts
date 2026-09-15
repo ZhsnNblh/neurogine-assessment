d2956ccc15a82596b35f274ff61ff4a31e098f10
Zaihusna Nabilah <217222@student.upm.edu.my>
2026-09-15 11:54:43 +0800

    feat: add product data model

---
+++ src/types/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  brand: string;
+  rating: number;
+  thumbnail: string;
+};
